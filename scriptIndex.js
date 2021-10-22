f19d28dc014f6a3c345b10146300f1b81f86a032
Bertinoti <jeffersonbertinoti@gmail.com>
2021-10-22 14:35:00 +0200

    try to do display

--- scriptIndex.js
+++ scriptIndex.js
@@ -3,23 +3,68 @@ var resToArr=[];
 var total;
 var holdTotal=0;
 var displayRes= document.getElementById('result');
-var divDisplay = document.getElementById('result');
+var displayOperation = document.getElementById('showOperation');
 var newP = document.createElement('p');
+var dosPs= document.createElement('p');
+var historic=[]
 
 function addNumber(num){
     result+= num;
     showRes(num);
 }
 
-function showRes(res){
-    newP.innerHTML += res;
+function showNUmber (){
+    newP.innerHTML = result;
     displayRes.appendChild(newP);
 }
 
+function showNumberUp(){
+    dosPs.innerHTML=''
+    contHistoric= historic.length
+    console.log(contHistoric)
+    if (contHistoric < 2) {
+        dosPs.innerHTML = `${resToArr[0]} ${resToArr[1]} `;
+        console.log(dosPs)
+    displayOperation.appendChild(dosPs)
+    }else{
+        dosPs.innerHTML = `${historic[0]} ${historic[1]} ${historic[2]} `;
+        console.log(dosPs)
+        displayOperation.appendChild(dosPs)
+    }
+}
+
+function showResult (){
+    
+    dosPs.innerHTML = `${historic[0]} ${historic[1]} ${historic[2]} `;
+    displayOperation.appendChild(dosPs)
+
+    newP.innerHTML = `${historic[3]} `;
+    displayOperation.appendChild(newP)
+}
+
+function showRes(res){
+    if( res == '+' || res == '-' || res == '*' || res == '%' || res == '/' || res == '+-'){
+        showNumberUp()
+    }else
+    if (res === '=') {
+        showResult()
+    }else{
+        showNUmber()
+    }
+
+}
+
 function addOperator(operator){
-    showRes(operator);
-    resToArr.push(result, operator);
-    result='';
+    if (resToArr.length >= 3 ) {
+        console.log(resToArr.length)
+        equal()
+        
+        
+    }else{
+        resToArr.push(result, operator);
+        showRes(operator);
+        result='';
+    }
     return resToArr;
 }
 
@@ -54,6 +99,9 @@ function equal(){
         holdTotal=total;
         aux=0;
     }
+    resToArr.push(holdTotal)
+    historic.push(resToArr);
+    console.log(historic)
 
     resToArr=[];
     result= holdTotal;
@@ -65,6 +113,7 @@ function clearAll(){
     result='';
     resToArr=[];
     newP.innerHTML= '';
+    dosPs.innerHTML= '';
 }
 
 
@@ -78,18 +127,19 @@ function visualiza_primero() {
     document.getElementById('segundo').style.visibility='hidden';
     document.getElementById('segundo').style.display='none';
     cuerpoweb.classList.toggle("oscuro");
- };
- function visualiza_segundo() {
+};
+function visualiza_segundo() {
     document.getElementById('segundo').style.visibility='visible';
     document.getElementById('segundo').style.display='block';
     document.getElementById('primero').style.visibility='hidden';
     document.getElementById('primero').style.display='none';
     cuerpoweb.classList.toggle("oscuro");
- };
-}
+};
+
 
 function convertSign() {
     if(result != '') {
         (Math.sign(result) == 1 || result == '0') ? result = -result : result = Math.abs(result);
+        newP.innerHTML= result;
     }
 }
