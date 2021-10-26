ddd2004c4826ab2e7d0ea8c3e766ac500d9d0194
Bertinoti <jeffersonbertinoti@gmail.com>
2021-10-26 15:09:59 +0200

    calculator Finished

--- scriptIndex.js
+++ scriptIndex.js
@@ -1,3 +1,4 @@
+//Global Variables
 var result='';
 var total;
 var holdTotal=0;
@@ -6,33 +7,30 @@ var historic=[];
 var cuerpoweb = document.body;
 var displayRes= document.getElementById('result');
 var displayOperation = document.getElementById('showOperation');
-//var primeravisualiza =document.getElementById("primero");
-//var segundavisualiza= document.getElementById("segunda");
 var pUno = document.createElement('div');
 var pDos= document.createElement('div');
 var contHist= historic.length
 var historicResult= historic[contHist-1]
-var disponibleOpe = false;
 var historico=document.getElementById("historico")
 
 
-
+// Add number and call function to display
 function addNumber(num){
-        result+= num;
-        console.log(result)
-        showRes(num);
-        disponibleOpe = true;
+    result+= num;
+    showRes(num);
 }
 
+// functions do display inputs
+//display one number 
 function showNumber (){
     pUno.innerHTML = result;
     displayRes.appendChild(pUno);
 }
 
+//display numbers and operators
 function showNumberUp(){
-    //console.log(contHist)
     if( contHist > 3){
-        pDos.innerHTML = ` ${historicResult[3]} `;
+        pDos.innerHTML = historicResult[3]
         displayOperation.appendChild(pDos);
         displayRes=""
     }else{
@@ -47,21 +45,22 @@ function showNumberUp(){
     }
 }
 
+// display result when add operations
 function showResult (){
-    contHist= historic.length
-    historicResult= historic[contHist-1]
-    pDos.innerHTML =`${historicResult[0]} ${historicResult[1]} ${historicResult[2]} =` ;
+    pDos.innerHTML = historic[0][3]
     displayOperation.appendChild(pDos);
-
-    pUno.innerHTML = `${historicResult[3]} `;
+    pUno.innerHTML = ''
     displayRes.appendChild(pUno);
 }
 
+// Display results when press equal
 function showResultEqual(){
     pDos= historicResult[4]
     displayOperation.appendChild(pDos)
 }
 
+
+//choose the right function to call
 function showRes(res){
     if( res == '+' || res == '-' || res == '*' || res == '%' || res == '/' || res == '+-'){
         showNumberUp();
@@ -71,54 +70,66 @@ function showRes(res){
     }else{
         showNumber();
     }
-
 }
 
-function mostrar (){
-    showHistoric()
-    cont = historic.length;
-    console.log(historic[cont - 1])
+function isOperator(value){
+    const operators= ['+', '-', '*', '/', '%']
+    return operators.includes(value)
 }
-function addOperator(operator){
-    if(disponibleOpe) {
-        if (resToArr.length >= 2 ) {
-            //console.log(resToArr.length)
-            equal()
-            resToArr.push(result, operator);
-            showRes(operator);
-            result='';
-            disponibleOpe = false
-        }else{
-            resToArr.push(result, operator);
-            showRes(operator);
-            result='';
-            disponibleOpe = false
-            }
-    }
-    
+
+//add operator
+/*function addOperator(operator){
+    if( resToArr[resToArr.length-1] ){}
+    if (resToArr.length >= 2 ) {
+        equal()
+        resToArr.push(result, operator);
+        showRes(operator);
+        resToArr.push(result, operator);
+    }else{
+        resToArr.push(result, operator);
+        showRes(operator);
+        result='';
+        }
     return resToArr;
+}*/
 
-    // if (resToArr.length >= 2 ) {
-    //     //console.log(resToArr.length)
-    //     equal()
-    //     resToArr.push(result, operator);
-    //     showRes(operator);
-    //     result='';
-    //     mostrar()
-    // }else{
-    //     //Comprovar si resultado es un numero
-    //     if( !isNaN( result.slice(0, -1) )  && (result !== '' || ) {
-    //         resToArr.push(result, operator);
-    //         showRes(operator);
-    //         result='';
-    //         mostrar()
-    //     } else {
-    //         resToArr.push(0, operator);
-    //         mostrar()
-    //     }
-    //}
-    //return resToArr;
+function addOperator(operator){
+    if( result !== ''){
+        resToArr.push(result)
+        result=''
+    }
+    if(resToArr.length <= 2) {
+        resToArr.push(operator);
+        equal()
+        //resToArr.push(result, operator);
+        showRes(operator);
+    }else
+    if (resToArr.length >= 3) {
+    //console.log(resToArr.length >= 3)
+        if (isOperator( resToArr[resToArr.length-1])) {
+        //console.log('before caso 2 operadores',resToArr)
+        delete resToArr[1]
+        //console.log('AFTER caso 2 operadores',resToArr)
+        resToArr[1] = operator;
+        delete resToArr[2]
+        resToArr = resToArr.filter((v)=> {if (v && v != '') return v } )
+        console.log(resToArr)
+        showRes(operator)
+    }else{
+        //console.log(resToArr)
+        equal()
+        showRes(operator);
+        //console.log(resToArr)
+        //console.log('caso 1 operadores con 2 numeros',resToArr)
+        }
+    }
+    return resToArr;
 }
+
+// function to calculate each operation
+// proccess all operation and return result
+// hold each result until finished all operation
+// add in Historic array all operation and result
 
 function equal(){
     if(result == '') {
@@ -161,24 +172,31 @@ function equal(){
     }
     resToArr.push(holdTotal)
     historic.push(resToArr);
-    console.log(historic)
-
-    mostrar()
-
+    showHistoric()
+    
     resToArr=[];
     result= holdTotal;
     total=null;
-    disponibleOpe = true
-    //newP.innerHTML += ` =  ${holdTotal}`;
 }
 
+//convert the Number sign
+function convertSign() {
+    if(result != '') {
+        (Math.sign(result) == 1 || result == '0') ? result = -result : result = Math.abs(result);
+        pUno.innerHTML= result;
+    }
+}
+
+// clean all arrays except Historic
 function clearAll(){
     result='';
+    holdTotal=0
     resToArr=[];
     pUno.innerHTML= '';
     pDos.innerHTML= '';
 }
 
+//change toggle button dark mode
 function visualiza_primero() {
     document.getElementById('primero').style.visibility='visible';
     document.getElementById('primero').style.display='block';
@@ -186,6 +204,8 @@ function visualiza_primero() {
     document.getElementById('segundo').style.display='none';
     cuerpoweb.classList.toggle("oscuro");
 };
+
+//change to Ligth mode
 function visualiza_segundo() {
     document.getElementById('segundo').style.visibility='visible';
     document.getElementById('segundo').style.display='block';
@@ -194,14 +214,7 @@ function visualiza_segundo() {
     cuerpoweb.classList.toggle("oscuro");
 };
 
-
-function convertSign() {
-    if(result != '') {
-        (Math.sign(result) == 1 || result == '0') ? result = -result : result = Math.abs(result);
-        pUno.innerHTML= result;
-    }
-}
-
+//Display Historic
 function oculto() {
     var desaparecer = document.getElementById("myDIV");
     if (desaparecer.style.display === "none") {
@@ -212,21 +225,20 @@ function oculto() {
     }
 }
 
-
+//Print array Historic
 function showHistoric(){
     resetHistorichtml()
-historic.forEach(element => {
-    let itemP=document.createElement("p")
 
-    itemP.innerHTML= `${element[0]} ${element[1]} ${element[2]} = ${element[3]}`
-    historico.appendChild(itemP)
-
-
-    console.log(itemP.innerHTML)
-});
+    let reverseHistoric= historic.reverse();
+    reverseHistoric.forEach(element => {
+        let itemP=document.createElement("p")
+        itemP.innerHTML= `${element[0]} ${element[1]} ${element[2]} = ${element[3]}`
+        historico.appendChild(itemP)
+    });
 }
 
+//clear display Historic
 function resetHistorichtml(){
-   while(historico.firstChild){
+    while(historico.firstChild){
     historico.removeChild(historico.lastChild)}
-   }
+}
